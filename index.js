5022d7a2c25d737a59b30a9931f8e8ca33852147
Damian Vigo <damiandanielvigo@gmail.com>
2022-05-26 20:04:44 -0300

    add-routes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+import 'dotenv/config';
+import './database/connectdb.js';
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ ok: true });
-});
+app.use('/', authRoutes);
 
-app.listen(5000, () => {
-  console.log('server online 🔥 http://localhost:5000');
+/* app.get('/', (req, res) => {
+  res.json({ ok: true });
+}); */
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('server online 🔥 http://localhost:' + PORT);
 });
